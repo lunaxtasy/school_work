870d7663182635bdbd1788f8ad2ec8dcb0cd7afd
Becky <55168393+lunaxtasy@users.noreply.github.com>
2019-10-09 23:00:00 -0400

    Assignment 4 Step 1

--- palindrome/palindrome.py
+++ palindrome/palindrome.py
@@ -8,7 +8,7 @@ def is_palindrome(word):
     """
     This function checks to see if an entered string is a palindrome or not.
 
-    Returns True for palindromes and False for non-palindromes and empty lines
+    Returns True for palindromes and False for non-palindromes and empty strings
     """
 
     #string checker, will raise ValueError if anything else but a string
